02e2bd9cdb3e6e01ed0f48913d0a8010fc4f91ae
DudeCalledZee <ineedtothink@gmail.com>
2021-07-05 09:37:16 +0100

    init project and add events + state enum

---
+++ src/test/java/com/zee/saydullaev/statemachine/StateMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zee.saydullaev.statemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StateMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
